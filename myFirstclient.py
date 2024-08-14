8409cc5ab14f53663ca46f9b20da46887cdcccf4
JoshilPatel <144334639+JoshilPatel@users.noreply.github.com>
2024-08-14 17:04:07 +0100

    Add files via upload

---
+++ myFirstclient.py
@@ -0,0 +1,17 @@
+import socket
+#Address of the raspbery pi pico w
+serverAddress=('192.168.0.126',2222)
+bufferSize=1024
+#Setting up the client
+UDPClient=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+
+while True:
+    cmd=input('What is your command? ')
+    #Encode the command
+    cmdEncoded=cmd.encode('utf-8')
+    #Send command to the server
+    UDPClient.sendto(cmdEncoded,serverAddress)
+    #Waiting for received confirmation from server
+    data,address=UDPClient.recvfrom(bufferSize)
+    dataDecoded=data.decode('utf-8')
+    print('MESSAGE FROM SERVER:',dataDecoded)
